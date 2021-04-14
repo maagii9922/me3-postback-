9984d37f5e47c24385f7355774f7c3f6e77e9eab
mandah <mandakh@bumanit.mn>
2021-04-14 11:23:07 +0800

    lll

--- me3/settings.py
+++ me3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!*voz7^k&!jpx3m4#b(yz(n+(v2uaut#609oi)p)q*b2oze(-y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["infinite-sands-98742.herokuapp.com"]
 
 
 # Application definition
