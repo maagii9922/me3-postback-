4671482966b06a20634f63397afc38320daed808
mandah <mandakh@bumanit.mn>
2021-04-14 10:56:50 +0800

    lll

---
+++ me3/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.views import generic
+
+
+def home(request):
+    return HttpResponse("hello")
+
+VERIFY_TOKEN='123456'
+
+class Botview(generic.View):
+    def get(self,request, *args,**kwargs):
+        if self.request.GET['hub.verify_token']==VERIFY_TOKEN:
+            return HttpResponse(self.request.GET['hub.challenge'])
+        else:
+            return HttpResponse('Error, invalid token')
